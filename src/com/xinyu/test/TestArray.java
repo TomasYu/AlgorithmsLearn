949e25d5d0ccf5bf15bddb68d655c59d409ea1de
caoxinyu <1455971532@qq.com>
2019-01-07 13:40:50 +0800

    增加：第二天的练习

--- src/com/xinyu/test/TestArray.java
+++ src/com/xinyu/test/TestArray.java
@@ -10,8 +10,10 @@ public class TestArray
 //        int [] testArray = new int[]{1,4,3,33,5,0,2,0,53,0,24,0};
 //        int [] testArray = new int[]{0,0,0};
 //        int [] testArray = new int[]{1,0};
-        int [] testArray = new int[]{0,2,1,223,43223,4234,234,324,3243,243,24};
-        moveZeroes(testArray);
+        int [] testArray = new int[]{0,1,2,2,3,0,4,2};
+//        moveZeroes(testArray);
+        int i = removeElement(testArray, 2);
+        System.out.println("i :" + i);
         printArray(testArray);
         System.out.println(testArray);
 
@@ -102,4 +104,80 @@ public class TestArray
             }
         }
     }
+
+    /**
+     * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+
+     不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+     元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+     示例 1:
+
+     给定 nums = [3,2,2,3], val = 3,
+
+     函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+     你不需要考虑数组中超出新长度后面的元素。
+     示例 2:
+
+     给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+
+     函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+     注意这五个元素可为任意顺序。
+
+     你不需要考虑数组中超出新长度后面的元素。
+     说明:
+
+     为什么返回数值是整数，但输出的答案是数组呢?
+
+     请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement(int[] nums, int val) {
+
+        //我的解决方法  9ms
+        if (nums ==null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        int temp = 0;
+        int j = 0;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != val) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
+        }
+        return j;
+
+
+        //别人的方法： 5ms
+//        if(nums.length==0)
+//            return 0;
+//        int i,j;
+//        for(i=0,j=nums.length-1;i<=j;) {
+//            if(nums[j]==val){
+//                j--;
+//            } else {
+//                if(nums[i]!=val)
+//                    i++;
+//                else {
+//                    nums[i]=nums[j];
+//                    j--;
+//                }
+//            }
+//        }
+//        j++;
+//        return j;
+
+    }
+
+
+
+
 }
