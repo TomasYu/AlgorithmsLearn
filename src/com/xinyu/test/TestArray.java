d4a763cd656b99611ed2cb2be44159705ac4e45c
caoxinyu <1455971532@qq.com>
2019-01-22 14:50:12 +0800

    增加：第十天的练习

--- src/com/xinyu/test/TestArray.java
+++ src/com/xinyu/test/TestArray.java
@@ -37,6 +37,14 @@ public class TestArray
         reverseVowels("hello");
 
 
+        /**
+         * test [1,2,4,3]
+         */
+
+        int maxArea = maxArea(new int[]{1, 2, 4, 3});
+        System.out.println("maxArea: "+maxArea);
+
+
         System.out.println("耗时："+(System.nanoTime() - l)+"纳秒");
         printArray(testArray);
         System.out.println(testArray);
@@ -886,6 +894,113 @@ public class TestArray
         char tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    }
+
+
+    /**
+     * 盛最多水的容器
+     给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+     说明：你不能倾斜容器，且 n 的值至少为 2。
+
+     备注：图片在pic/question_11.jpg
+     图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+     示例:
+
+     输入: [1,8,6,2,5,4,8,3,7]
+     输出: 49
+     */
+
+    public static int maxArea(int[] height) {
+//        int length = height.length;
+//        if (length == 0) {
+//            return 0;
+//        }else if (length == 1){
+//            return height[0];
+//        }
+//
+//        int max = 0;
+//        for (int i =0, j =length -1; j > i ;  ){
+//            int minHeight = Math.min(height[i], height[j]);
+//            max = Math.max(max,minHeight * (j -i));
+//            if (height[i] > height[j]) {
+//                j--;
+//            }else {
+//                i++;
+//            }
+//        }
+//        return max;
+
+
+
+        int length = height.length;
+        if (length == 0) {
+            return 0;
+        }else if (length == 1){
+            return height[0];
+        }
+
+        int max = 0;
+        for (int i =0, j =length -1; j > i ;  ){
+            int start = height[i];
+            int end = height[j];
+            int minHeight = Math.min(start, end);
+            max = Math.max(max,minHeight * (j -i));
+//            if (start > end) {
+//                while (height[--j]<end && j> i);
+//            }else {
+//                while (height[++i]<end && j> i);
+//            }
+
+//            if (start > end) {
+//                j--;
+//                while (height[j]<end && j> i){
+//                    j--;
+//                };
+//            }else {
+//                i++;
+//                while (height[i]<start && j> i){
+//                    i++;
+//                };
+//            }
+
+            if (start > end) {
+                do {
+                    j--;
+                }while (height[j]<end && j> i);
+            }else {
+                do {
+                    i++;
+                }while (height[i]<start && j> i);
+            }
+        }
+        return max;
+
+
+
+
+        //网上最快的
+//        class Solution {
+//            public int maxArea(int[] height) {
+//                int max = 0, nmax = 0;
+//                int i = 0, j = height.length - 1;
+//                int a;
+//                while(i < j){
+//                    if (height[i] <= height[j]){
+//                        nmax =  height[i] * (j - i);
+//                        a = height[i];
+//                        while(height[++i] <= a && i < j);
+//                    }else{
+//                        nmax =  height[j] * (j - i);
+//                        a = height[j];
+//                        while(height[--j] <= a && i < j);
+//                    }
+//                    if (nmax > max)max = nmax;
+//                }
+//                return max;
+//            }
+//        }
+
     }
 
 
