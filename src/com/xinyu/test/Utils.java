bf9d55b0bbef2711a6af4ec381e67bb414572019
caoxinyu <1455971532@qq.com>
2019-01-28 15:57:37 +0800

    增加：第十二天的练习

--- src/com/xinyu/test/Utils.java
+++ src/com/xinyu/test/Utils.java
@@ -1,5 +1,7 @@
 package com.xinyu.test;
 
+import java.util.List;
+
 /**
  * Created by zy on 2019/1/24.
  */
@@ -20,5 +22,49 @@ public class Utils {
         stringBuffer.append("]");
         System.out.println("数组："+stringBuffer.toString());
     }
+
+    /**
+     * 打印数组
+     * @param list
+     */
+    public static void printList(List list){
+        StringBuilder stringBuffer = new StringBuilder("[");
+        for (int i = 0; i < list.size(); i++) {
+            Object o = list.get(i);
+            if (o instanceof List) {
+                printList((List) o);
+            }else {
+                stringBuffer.append(o);
+                if (i != list.size() - 1) {
+                    stringBuffer.append(",");
+                }
+            }
+
+        }
+        stringBuffer.append("]");
+        System.out.println("数组："+stringBuffer.toString());
+    }
+
+    /**
+     * 冒泡排序
+     * @param nums
+     */
+    public static void MaoPao(int nums[]) {
+        if (nums ==null) {
+            return;
+        }
+        int len = nums.length;
+        int temp;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] > nums[j]) {
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+    }
+
 
 }
