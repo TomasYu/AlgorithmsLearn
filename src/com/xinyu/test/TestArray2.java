009812c49beb79da6583014d8034f57944e32cdd
caoxinyu <1455971532@qq.com>
2019-02-22 23:19:49 +0800

    增加：第十六天的练习

--- src/com/xinyu/test/TestArray2.java
+++ src/com/xinyu/test/TestArray2.java
@@ -119,10 +119,29 @@ public class TestArray2 {
          输入: [1,1,1,3,3,4,3,2,4,2]
          输出: true
          */
-        int[] testArray = new int[]{};
-        System.out.println(containsDuplicate(testArray));
+//        int[] testArray = new int[]{};
+//        System.out.println(containsDuplicate(testArray));
 
 
+        /**
+         * 只出现一次的数字
+         给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+         说明：
+
+         你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+         示例 1:
+
+         输入: [2,2,1]
+         输出: 1
+         示例 2:
+
+         输入: [4,1,2,1,2]
+         输出: 4
+         */
+        int []arr = new int[]{1,1,2};
+        System.out.println(singleNumber(arr));;
     }
 
 
@@ -531,6 +550,72 @@ public class TestArray2 {
 //        }
 //        return false;
 
+
+    }
+
+    public static int singleNumber(int[] nums) {
+
+//第一版
+//        if (nums == null) {
+//            return -1;
+//        }
+//        int length = nums.length  ;
+//        if (length == 1) {
+//            return nums[0];
+//        }
+//        int numCount;
+//        int j,k;
+//        for (int i = 0 ; i< length  ;){
+//             j = k = i + 1;
+//             numCount = 0;
+//            for (; j<length; j++){
+//                if (nums[i] == nums[j]){
+//                    numCount ++;
+//                    //交换
+//                    nums[j]  = nums[k];
+//                    nums[k] = nums[i];
+//                    k++;
+//                }
+//            }
+//            if (numCount == 0){
+//                return nums[i];
+//            }
+//            i = k;
+//
+//        }
+//        return nums[length-1];
+
+        //第二版
+//        if (nums == null) {
+//            return -1;
+//        }
+//        int length = nums.length  ;
+//        if (length == 1) {
+//            return nums[0];
+//        }
+//        Arrays.sort(nums);
+//        for (int i =0,j,k; i< length; ){
+//            j = k = i+1;
+//            if (i != length -1) {
+//                if (nums[j] != nums[i]) {
+//                    return nums[i];
+//                }else {
+//                    while (k< length && nums[k++] == nums[i]){}
+//                    i = k-1;
+//                }
+//            }else {
+//                i++;
+//            }
+//        }
+//        return nums[length -1];
+
+
+        for (int i = 1, len = nums.length; i < len; i += 2) {
+            nums[0] ^= nums[i] ^ nums[i+1];
+        }
+        return nums[0];
+
+        //总结：  所有的数字都是二进制，时刻铭记
 
     }
 }
