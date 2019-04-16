85831bb1c25a1d35c633f611d19706d7289d3597
caoxinyu <1455971532@qq.com>
2019-04-16 22:05:53 +0800

    增加：第二十四天的练习

---
+++ src/com/xinyu/test/TestArray7.java
@@ -0,0 +1,54 @@
+package com.xinyu.test;
+
+import java.util.Arrays;
+
+/**
+ * Created by zy on 2019/4/16.
+ */
+public class TestArray7 {
+
+    public static void main(String[] args) {
+//        System.out.println(majorityElement(new int[]{1,2,1,2,2}));
+        System.out.println(majorityElement(new int[]{1,1,2,2,3,2,2}));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int count = 1;
+        int curNum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == curNum) {
+                count++;
+                if (count > nums.length /2) {
+                    return curNum;
+                }
+            }else {
+                curNum = nums[i];
+                count = 1;
+            }
+        }
+        return curNum;
+
+          //我喜欢的算法
+//        int ans = nums[0], count = 1;
+//        for(int i = 1; i < nums.length; ++i) {
+//            if(count == 0) {
+//                count++;
+//                ans = nums[i];
+//            }
+//            else if(nums[i] == ans) {
+//                count++;
+//            }
+//            else {
+//                count--;
+//            }
+//        }
+//        return ans;
+
+        //网上最快的算法：
+//        Arrays.sort(nums);
+//        return nums[nums.length/2];
+
+
+    }
+}
