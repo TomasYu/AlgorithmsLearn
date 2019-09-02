db8dea63f3708a43b6314e276c34e0d584ecc2d3
caoxinyu <1455971532@qq.com>
2019-09-02 16:42:44 +0800

    增加注释，看懂了。很开心。别人的算法，真的很棒。

--- src/com/xinyu/test/TestArray13.java
+++ src/com/xinyu/test/TestArray13.java
@@ -6,7 +6,7 @@ package com.xinyu.test;
 public class TestArray13 {
 
     public static void main(String[] args) {
-        System.out.println(findMedianSortedArrays(new int[]{1,2},new int[]{-1,3}));
+        System.out.println(findMedianSortedArrays(new int[]{-1,0,3,8,9,10},new int[]{4,7,12,19,20}));
     }
 
 
@@ -105,23 +105,41 @@ public class TestArray13 {
         return (small + big) / 2.0;
     }
 
+
+    /**
+     *
+     * @param nums1 数组1
+     * @param index1 表示数组1从第几位开始找
+     * @param nums2 数组2
+     * @param index2  表示数组2 从第几位开始找
+     * @param k  k表示要找第几位，注意k 代表的不是索引，是索引加1
+     * @return
+     */
     private static int findKth(int[] nums1, int index1, int[] nums2, int index2, int k) {
+        // 如果第一个数组查找的位置是末尾，那么说明要找的数据不再数组1里面，那么直接从数组2里面找就可以。
         if (index1 == nums1.length) {
             return nums2[index2 + k - 1];
         }
+
+        // 如果第二个数组查找的位置是末尾，那么说明要找的数据不再数组1里面，那么直接从数组1里面找就可以。
         if (index2 == nums2.length) {
             return nums1[index1 + k - 1];
         }
+        //如果要找当前第一位，那么直接返回当前两个数组的最小值
         if (k == 1) {
             return Math.min(nums1[index1], nums2[index2]);
         }
 
+        // 找到第k  位对应的，两个数组的数值  每次排除掉k/2 个数
         int num1 = index1 + k / 2 - 1 < nums1.length ? nums1[index1 + k / 2 - 1] : Integer.MAX_VALUE;
         int num2 = index2 + k / 2 - 1 < nums2.length ? nums2[index2 + k / 2 - 1] : Integer.MAX_VALUE;
 
+        // 如果数组1 的比较小，那么数组1的查找开始位置要往前挪动k/2 个位置（即一下子排除了k/2 个元素），同时要找的位置减去k/2
+        //注意 数学里面的k - k / 2  和java 里面的k - k / 2 不是等值的
         if (num1 < num2) {
             return findKth(nums1, index1 + k / 2, nums2, index2, k - k / 2);
         }
+        // 如果数组2 的比较小，那么数组2的查找开始位置要往前挪动k/2 个位置（即一下子排除了k/2 个元素），同时要找的位置减去k/2
         return findKth(nums1, index1, nums2, index2 + k / 2, k - k / 2);
     }
 
