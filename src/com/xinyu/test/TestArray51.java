f0a87ee82e89be9dc4747639762462b35142b01e
caoxinyu <1455971532@qq.com>
2020-06-15 22:49:38 +0800

    二分法需要去练习

---
+++ src/com/xinyu/test/TestArray51.java
@@ -0,0 +1,20 @@
+package com.xinyu.test;
+
+public class TestArray51 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,4,7,9,20};
+        int targat = 8;
+        System.out.println(getMaxLess(arr, 8));
+        System.out.println(getMaxMore(arr, 8));
+    }
+
+    public static int getMaxLess(int[] arr, int targe){
+
+        return 1;
+    }
+
+    public static int getMaxMore(int[] arr, int targe){
+        return -1;
+    }
+}
