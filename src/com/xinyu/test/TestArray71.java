18c308fb9fdf15da696bccd55efd318bd48cf8ea
caoxinyu <caoxinyu@xiaomi.com>
2021-12-01 14:04:44 +0800

    下一个最大的书数

--- src/com/xinyu/test/TestArray71.java
+++ src/com/xinyu/test/TestArray71.java
@@ -1,4 +1,83 @@
 package com.xinyu.test;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+
 public class TestArray71 {
+    /**
+     * 下一个更大元素 I
+     *
+     *
+     * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+     *
+     * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+     *
+     * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+     *
+     *  
+     *
+     * 示例 1:
+     *
+     * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * 输出: [-1,3,-1]
+     * 解释:
+     *     对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+     *     对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+     *     对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * 示例 2:
+     *
+     * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+     * 输出: [3,-1]
+     * 解释:
+     *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+     *     对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     *  
+     *
+     * 提示：
+     *
+     * 1 <= nums1.length <= nums2.length <= 1000
+     * 0 <= nums1[i], nums2[i] <= 104
+     * nums1和nums2中所有整数 互不相同
+     * nums1 中的所有整数同样出现在 nums2 中
+     *  
+     *
+     * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     *
+     *
+     *
+     */
+
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] rst = new int[nums1.length];
+        Deque<Integer> deque = new ArrayDeque();
+        HashMap<Integer,Integer> result = new HashMap<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!deque.isEmpty() && nums2[i] > deque.peek()){
+                deque.pop();
+            }
+            result.put(nums2[i],deque.isEmpty() ? -1 : deque.peek());
+            deque.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            rst[i] = result.get(nums1[i]);
+        }
+        return rst;
+
+    }
+
+    public static void main(String[] args) {
+        TestArray71 testArray71 = new TestArray71();
+        int[] ints = testArray71.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2});
+        System.out.print("ints  " +  Arrays.toString(ints));
+    }
 }
+
