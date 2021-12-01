699e046aeaa311fd0ddec105a458e94da5c1d8e0
caoxinyu <caoxinyu@xiaomi.com>
2021-12-01 14:15:06 +0800

    增加新题目

---
+++ src/com/xinyu/test/TestArray72.java
@@ -0,0 +1,19 @@
+package com.xinyu.test;
+
+public class TestArray72 {
+
+    public static void main(String[] args) {
+        TestArray72 testArray72 = new TestArray72();
+        int i = testArray72.maxPoints(new int[][]{{1, 1}, {2, 2}, {3, 3}});
+        System.out.println(i);
+    }
+
+
+    /**
+     * 149. 直线上最多的点数
+     */
+
+    public int maxPoints(int[][] points) {
+        return 3;
+    }
+}
