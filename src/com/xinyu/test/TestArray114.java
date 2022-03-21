b329cb0820492328c5fde361af10938e8e162dac
caoxinyu <caoxinyu@xiaomi.com>
2022-03-21 21:44:14 +0800

    https://leetcode-cn.com/problems/search-insert-position/

---
+++ src/com/xinyu/test/TestArray114.java
@@ -0,0 +1,85 @@
+package com.xinyu.test;
+
+public class TestArray114 {
+    /**
+     * 搜索插入位置
+     *
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * 请必须使用时间复杂度为 O(log n) 的算法。
+     *
+     *  
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [1,3,5,6], target = 5
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: nums = [1,3,5,6], target = 2
+     * 输出: 1
+     * 示例 3:
+     *
+     * 输入: nums = [1,3,5,6], target = 7
+     * 输出: 4
+     * 示例 4:
+     *
+     * 输入: nums = [1,3,5,6], target = 0
+     * 输出: 0
+     * 示例 5:
+     *
+     * 输入: nums = [1], target = 0
+     * 输出: 0
+     *  
+     *
+     * 提示:
+     *
+     * 1 <= nums.length <= 104
+     * -104 <= nums[i] <= 104
+     * nums 为无重复元素的升序排列数组
+     * -104 <= target <= 104
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/search-insert-position
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1,mid = 0;
+        while (start < end){
+            mid = (end - start) / 2 + start;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target){
+                end = mid -1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        if (nums[start] >= target){
+            return start;
+        }else {
+            return start + 1;
+        }
+    }
+
+    public int searchInsert2(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(searchInsert(new int[]{1,3,5,6},2));
+    }
+}
