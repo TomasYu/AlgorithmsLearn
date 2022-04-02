e4b81a26b9408fb072304fa4a29e1217eab64dc9
caoxinyu <caoxinyu@xiaomi.com>
2022-04-02 22:15:34 +0800

    add file

---
+++ src/com/xinyu/test/TestStream.java
@@ -0,0 +1,28 @@
+package com.xinyu.test;
+
+public class TestStream {
+
+    /**
+     * <img width="640" height="310" src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/flatMap.png" alt="">
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+
+    }
+
+    /**
+     * <img width="640" height="310" src="../../../../pic/question_11.jpg" alt="">
+     */
+    public static void test(){
+
+    }
+
+    /**
+     * jfjasjf
+     */
+    public void in2t (){
+
+    }
+}
---
+++ src/com/xinyu/test/TestStream2.java
@@ -0,0 +1,13 @@
+package com.xinyu.test;
+
+public class TestStream2 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+    public static void test(){
+
+    }
+}
