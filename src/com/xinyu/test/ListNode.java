3559561f482b691e28fb75d09f1c20d84a8c8348
caoxinyu <caoxinyu@xiaomi.com>
2022-04-07 10:36:07 +0800

    https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/

--- src/com/xinyu/test/ListNode.java
+++ src/com/xinyu/test/ListNode.java
@@ -4,4 +4,9 @@ public class ListNode {
      int val;
     ListNode next;
     ListNode(int x) { val = x; }
+
+    public ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
+    }
 }
--- src/com/xinyu/test/TestTreeNode2.java
+++ src/com/xinyu/test/TestTreeNode2.java
@@ -29,12 +29,74 @@ public class TestTreeNode2 {
      */
 
     public static void main(String[] args) {
+        TestTreeNode2 testNode2 = new TestTreeNode2();
+        ListNode listNode = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode33 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode44 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode33;
+        listNode33.next = listNode4;
+        listNode4.next = listNode44;
+        listNode44.next = listNode5;
 
+        testNode2.deleteDuplicates(listNode);
 
 
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-        return null;
+
+        if (head == null) {
+            return null;
+        }
+        ListNode dummyNode = new ListNode(-1);
+        ListNode cur = dummyNode;
+        dummyNode.next = head;
+        int dumpNum = -1000;
+        while (cur.next != null){
+            if (dumpNum != -1000) {
+                if (cur.next.val == dumpNum){
+                    cur.next = cur.next.next;
+                }else {
+                    dumpNum = -1;
+                }
+            }else {
+                if (cur.next.next != null && cur.next.val == cur.next.next.val){
+                    dumpNum = cur.next.val;
+                }else {
+                    cur = cur.next;
+                }
+            }
+        }
+        return dummyNode.next;
     }
+
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
 }
