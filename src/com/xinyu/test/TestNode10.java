5e8a02b44ff6936a8848a008b4e273d70c53b2c3
caoxinyu <caoxinyu@xiaomi.com>
2022-04-13 22:43:39 +0800

    https://leetcode-cn.com/problems/palindrome-linked-list-lcci/

---
+++ src/com/xinyu/test/TestNode10.java
@@ -0,0 +1,12 @@
+package com.xinyu.test;
+
+public class TestNode10 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        return false;
+
+    }
+}
