7c277fed2c3d3468503a092385ef9cbe52a76308
caoxinyu <caoxinyu@xiaomi.com>
2022-04-13 22:51:26 +0800

    update

--- src/com/xinyu/test/TestNode10.java
+++ src/com/xinyu/test/TestNode10.java
@@ -5,6 +5,18 @@ public class TestNode10 {
 
     }
 
+    /**
+     * 编写一个函数，检查输入的链表是否是回文的。
+     * 输入： 1->2
+     * 输出： false
+     * 示例 2：
+     *
+     * 输入： 1->2->2->1
+     * 输出： true
+     *
+     * @param head
+     * @return
+     */
     public boolean isPalindrome(ListNode head) {
         return false;
 
