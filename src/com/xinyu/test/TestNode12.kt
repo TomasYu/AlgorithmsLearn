a04bab1bad6e8d698f7cba329eb28e7271660f42
caoxinyu <caoxinyu@xiaomi.com>
2022-04-15 23:02:28 +0800

    https://leetcode-cn.com/problems/palindrome-linked-list-lcci/

---
+++ src/com/xinyu/test/TestNode12.kt
@@ -0,0 +1,14 @@
+package com.xinyu.test
+
+fun main() {
+    isPalindrome(null)
+}
+
+
+/**
+ * https://leetcode-cn.com/problems/palindrome-linked-list-lcci/solution/di-gui-zhan-deng-3chong-jie-jue-fang-shi-zui-hao-2/
+ */
+
+fun isPalindrome(head: ListNode?): Boolean {
+    return false
+}
