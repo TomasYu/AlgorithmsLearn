ad14f39c23317affee80b0cc270a171694c0b9b3
caoxinyu <caoxinyu@xiaomi.com>
2022-04-20 16:50:21 +0800

    https://leetcode-cn.com/problems/binode-lcci/

--- src/com/xinyu/test/TestNode16.kt
+++ src/com/xinyu/test/TestNode16.kt
@@ -46,7 +46,7 @@ fun getKthFromEnd2(head: ListNode?, k: Int): ListNode? {
     for (i in 1..k) {
         fast = fast?.next
     }
-    while (fast != null){
+    while (fast != null) {
         fast = fast.next
         slow = slow?.next
     }
@@ -54,7 +54,61 @@ fun getKthFromEnd2(head: ListNode?, k: Int): ListNode? {
 }
 
 fun main() {
-    for (i in 0..2){
+    for (i in 0..2) {
         println(i)
     }
+    val convertBiNode = convertBiNode(TreeNode(4,
+            TreeNode(2, TreeNode(1, TreeNode(0, null, null), null), TreeNode(3, null, null)),
+            TreeNode(5, null, TreeNode(6, null, null))))
+    convertBiNode?.`val`
 }
+
+
+/**
+ * 二叉树数据结构TreeNode可用来表示单向链表（其中left置空，right为下一个链表节点）。实现一个方法，把二叉搜索树转换为单向链表，要求依然符合二叉搜索树的性质，转换操作应是原址的，也就是在原始的二叉搜索树上直接修改。
+
+返回转换后的单向链表的头节点。
+
+注意：本题相对原题稍作改动
+
+ 
+
+示例：
+
+输入： [4,2,5,1,3,null,6,0]
+输出： [0,null,1,null,2,null,3,null,4,null,5,null,6]
+提示：
+
+节点数量不会超过 100000。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binode-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+fun convertBiNode(root: TreeNode?): TreeNode? {
+    convertBiNode2(root)
+    return head
+}
+
+var head: TreeNode? = null
+var pre: TreeNode? = null
+fun convertBiNode2(root: TreeNode?) {
+    if (root == null) {
+        return
+    }
+    val left = root.left
+    val right = root.right
+    convertBiNode2(left)
+    if (head == null) {
+        head = root
+        pre = head
+    } else {
+        pre?.right = root
+        pre = root
+    }
+    root.left = null
+    convertBiNode2(right)
+}
+
