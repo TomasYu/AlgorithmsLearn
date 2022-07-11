6fc29aed28f5934c202d2d742112131d761bbf87
caoxinyu <caoxinyu@xiaomi.com>
2022-07-11 21:50:08 +0800

    https://leetcode.cn/problems/three-steps-problem-lcci/
    2

--- src/com/xinyu/test/TestInt14.kt
+++ src/com/xinyu/test/TestInt14.kt
@@ -17,5 +17,41 @@ class TestInt14 {
     提示:
 
     n范围在[1, 1000000]之间
+
+
+    f(n)表示，上到第n+1个台阶有多少种方法
+    一次可以上1阶、2阶、3阶，所有f(n)仅与f(n-3)、f(n-2)、f(n-1)有关
+    f(n) = f(n-3) + f(n-2) + f(n-1)
+
+    作者：huyii
+    链接：https://leetcode.cn/problems/three-steps-problem-lcci/solution/mian-shi-ti-0801-san-bu-wen-ti-by-huyii-s5u6/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
+    fun waysToStep(n: Int): Int {
+        when (n) {
+            1 -> {
+                return 1
+            }
+            2 -> {
+                return 2
+            }
+            3 -> {
+                return 4
+            }
+        }
+        if (n <= 1) return 1
+        val dp = Array(n + 1) { 0 }
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+        for (i in 4..n) {
+            dp[i] += ((dp[i - 1] + dp[i - 2]) % 1000000007 + dp[i - 3]) % 1000000007
+        }
+        return dp[n]
+    }
+}
+
+fun main() {
+    println(TestInt14().waysToStep(61))
 }
