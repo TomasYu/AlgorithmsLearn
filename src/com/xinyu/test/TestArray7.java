2cba71fec6b0d241821ebe57e81e9b51d08cebdc
caoxinyu <caoxinyu@xiaomi.com>
2022-08-03 21:08:25 +0800

    csdn 反馈问题处理

--- src/com/xinyu/test/TestArray7.java
+++ src/com/xinyu/test/TestArray7.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 public class TestArray7 {
 
     public static void main(String[] args) {
-//        System.out.println(majorityElement(new int[]{1,2,1,2,2}));
-        System.out.println(majorityElement(new int[]{1,1,2,2,3,2,2}));
+        System.out.println(majorityElement(new int[]{1,2,2,3,4}));
+        System.out.println(majorityElement2(new int[]{1,2,2,3,4}));
     }
 
     public static int majorityElement(int[] nums) {
@@ -44,6 +44,32 @@ public class TestArray7 {
 //            }
 //        }
 //        return ans;
+
+        //网上最快的算法：
+//        Arrays.sort(nums);
+//        return nums[nums.length/2];
+
+
+    }
+
+
+    public static int majorityElement2(int[] nums) {
+
+        //我喜欢的算法
+        int ans = nums[0], count = 1;
+        for(int i = 1; i < nums.length; ++i) {
+            if(count == 0) {
+                count++;
+                ans = nums[i];
+            }
+            else if(nums[i] == ans) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return ans;
 
         //网上最快的算法：
 //        Arrays.sort(nums);
