e25342dd486337836751b43a19cc12411e7507d8
caoxinyu <caoxinyu@xiaomi.com>
2022-09-08 22:38:49 +0800

    https://leetcode.cn/problems/binary-tree-paths

---
+++ src/com/xinyu/test/TestTreeNode4.kt
@@ -0,0 +1,71 @@
+package com.xinyu.test
+
+class TestTreeNode4 {
+    /**
+     * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+
+    叶子节点 是指没有子节点的节点。
+
+     
+    示例 1：
+
+
+    输入：root = [1,2,3,null,5]
+    输出：["1->2->5","1->3"]
+    示例 2：
+
+    输入：root = [1]
+    输出：["1"]
+     
+
+    提示：
+
+    树中节点的数目在范围 [1, 100] 内
+    -100 <= Node.val <= 100
+
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode.cn/problems/binary-tree-paths
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    fun binaryTreePaths(root: TreeNode?): List<String> {
+        val result = mutableListOf<String>()
+        dfs(root, StringBuffer(), result)
+        return result
+    }
+
+    /**
+     * 1. StringBuffer.delete(path.length - 2, path.length)
+     * 前包后不包
+     */
+    internal fun dfs(root: TreeNode?, path: StringBuffer, result: MutableList<String>) {
+        root?.let {
+            path.append(root.`val`.toString())
+            if (root.left == null && root.right == null) {
+                result.add(path.toString())
+                val index = path.lastIndexOf("->")
+                path.delete(index,path.length)
+                return
+            }
+            root.left?.let {
+                path.append("->")
+                dfs(root.left, path, result)
+                val index = path.lastIndexOf("->")
+                path.delete(index,path.length)
+            }
+
+            root.right?.let {
+                path.append("->")
+                dfs(root.right, path, result)
+                val index = path.lastIndexOf("->")
+                path.delete(index,path.length)
+            }
+//            val index = path.lastIndexOf("->")
+//            path.delete(index,path.length)
+        }
+    }
+}
+
+fun main() {
+    println(TestTreeNode4().binaryTreePaths(TreeNode(1, TreeNode(2, null, TreeNode(5, null, null)), TreeNode(3))))
+}
---
+++ src/com/xinyu/test/TestTreeNode5.kt
@@ -0,0 +1,65 @@
+package com.xinyu.test
+
+class TestTreeNode5 {
+    /**
+     * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+
+    叶子节点 是指没有子节点的节点。
+
+     
+    示例 1：
+
+
+    输入：root = [1,2,3,null,5]
+    输出：["1->2->5","1->3"]
+    示例 2：
+
+    输入：root = [1]
+    输出：["1"]
+     
+
+    提示：
+
+    树中节点的数目在范围 [1, 100] 内
+    -100 <= Node.val <= 100
+
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode.cn/problems/binary-tree-paths
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    fun binaryTreePaths(root: TreeNode?): List<String> {
+        val result = mutableListOf<String>()
+        dfs(root, mutableListOf(), result)
+        return result
+    }
+
+    /**
+     * 1. StringBuffer.delete(path.length - 2, path.length)
+     * 前包后不包
+     */
+    internal fun dfs(root: TreeNode?, path: MutableList<Int>, result: MutableList<String>) {
+        root?.let {
+            if (root.left == null && root.right == null) {
+                path.add(root.`val`)
+                result.add(path.joinToString("->"))
+                path.removeAt(path.size - 1)
+                return
+            }
+            path.add(root.`val`)
+            root.left?.let {
+                dfs(root.left, path, result)
+            }
+
+            root.right?.let {
+                dfs(root.right, path, result)
+            }
+            path.removeAt(path.size - 1)
+
+        }
+    }
+}
+
+fun main() {
+    println(TestTreeNode5().binaryTreePaths(TreeNode(1, TreeNode(2, null, TreeNode(5, null, null)), TreeNode(3))))
+}
