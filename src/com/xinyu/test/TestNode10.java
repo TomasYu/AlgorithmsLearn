449955bf127a915b8217e6ba51b88221552f9036
caoxinyu <caoxinyu@xiaomi.com>
2023-09-04 19:39:08 +0800

    注释掉

--- src/com/xinyu/test/TestNode10.java
+++ src/com/xinyu/test/TestNode10.java
@@ -1,24 +1,24 @@
-package com.xinyu.test;
-
-public class TestNode10 {
-    public static void main(String[] args) {
-
-    }
-
-    /**
-     * 编写一个函数，检查输入的链表是否是回文的。
-     * 输入： 1->2
-     * 输出： false
-     * 示例 2：
-     *
-     * 输入： 1->2->2->1
-     * 输出： true
-     *
-     * @param head
-     * @return
-     */
-    public boolean isPalindrome(ListNode head) {
-        return false;
-
-    }
-}
+//package com.xinyu.test;
+//
+//public class TestNode10 {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    /**
+//     * 编写一个函数，检查输入的链表是否是回文的。
+//     * 输入： 1->2
+//     * 输出： false
+//     * 示例 2：
+//     *
+//     * 输入： 1->2->2->1
+//     * 输出： true
+//     *
+//     * @param head
+//     * @return
+//     */
+//    public boolean isPalindrome(ListNode head) {
+//        return false;
+//
+//    }
+//}
