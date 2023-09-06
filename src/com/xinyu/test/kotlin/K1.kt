bdc6575a1fd7bded6550d9c55b2f10e071577b33
caoxinyu <caoxinyu@xiaomi.com>
2023-09-06 18:57:55 +0800

    add test

---
+++ src/com/xinyu/test/kotlin/K1.kt
@@ -0,0 +1,52 @@
+package com.xinyu.test.kotlin
+
+import kotlin.coroutines.*
+
+class K1 {
+
+}
+
+fun main() {
+    val coroutine = suspend {
+        println("suspend")
+        "aaa"
+    }.createCoroutine(object : Continuation<String> {
+        override val context: CoroutineContext
+            get() = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<String>) {
+            println(result.getOrNull())
+        }
+
+    })
+
+    coroutine.resume(Unit)
+
+
+    suspend {
+        println("suspend2")
+        "bbbb"
+    }.startCoroutine(object : Continuation<String> {
+        override val context: CoroutineContext
+            get() = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<String>) {
+            println(result.getOrNull())
+        }
+
+    })
+
+
+    suspend {
+        println("suspend2")
+        "bbbb"
+    }.startCoroutine(object : Continuation<String> {
+        override val context: CoroutineContext
+            get() = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<String>) {
+            println(result.getOrNull())
+        }
+
+    })
+}
