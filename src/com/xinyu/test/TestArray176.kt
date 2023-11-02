243bd04ea8dd2bc50d3e2eda584a9441c89153f4
caoxinyu <caoxinyu@xiaomi.com>
2023-11-02 16:37:48 +0800

    温故而知新2

--- src/com/xinyu/test/TestArray176.kt
+++ src/com/xinyu/test/TestArray176.kt
@@ -39,7 +39,7 @@ class TestArray176 {
                 while (right < nums.size) {
                     if (nums[right] == 0) {
                         right++
-                    }else{
+                    } else {
                         nums[left] = nums[right].apply {
                             nums[right] = nums[left]
                         }
@@ -52,6 +52,25 @@ class TestArray176 {
                 }
             }
             left++
+        }
+    }
+
+
+    fun moveZeroes2(nums: IntArray) {
+        //left左边是整理好的索引
+        var left = 0
+        //right 是left 右边当前第一个非0的索引
+        var right = 0
+        while (right < nums.size) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right].apply {
+                    nums[right] = nums[left]
+                }
+                left++
+                right++
+            } else {
+                right++
+            }
         }
     }
 }
@@ -69,12 +88,12 @@ fun main() {
 //
 //    println("$a $b")
 
-    var a = P(22,"a")
-    var b = P(28,"b")
+    var a = P(22, "a")
+    var b = P(28, "b")
     a = b.apply {
         b = a
     }
     println("${a.name} ${b.name}")
 
-    TestArray176().moveZeroes(intArrayOf(0,1,0,3,12))
+    TestArray176().moveZeroes(intArrayOf(0, 1, 0, 3, 12))
 }
